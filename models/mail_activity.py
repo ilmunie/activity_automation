1049ba1f9d9d307a7c263e04f61ac133838b3007
sapel <santiago.apel@eynes.com.ar>
2024-03-25 14:18:03 -0300

    [FIX] PREVENT DUPLICATION FOLLOWER

--- models/mail_activity.py
+++ models/mail_activity.py
@@ -30,3 +30,18 @@ class MailActivity(models.Model):
             if not activitys:
                 vals_list_to_create.append(act_to_create)
         return super(MailActivity, self).create(vals_list_to_create)
+
+class Followers(models.Model):
+    _inherit = 'mail.followers'
+
+    @api.model
+    def create(self, vals):
+        if 'res_model' in vals and 'res_id' in vals and 'partner_id' in vals:
+            dups = self.env['mail.followers'].search([('res_model', '=',vals.get('res_model')),
+                                               ('res_id', '=', vals.get('res_id')),
+                                               ('partner_id', '=', vals.get('partner_id'))])
+            if len(dups):
+                for p in dups:
+                    p.unlink()
+        res = super(Followers, self).create(vals)
+        return res
